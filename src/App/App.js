00e2bbe43e65b490e3bf3a27dba1e478847ffd48
Jamie <31694@ma-web.nl>
2022-09-21 12:53:50 +0200

    Count card

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import CounterCard from "../components/CounterCard/CounterCard";
+import "./App.css";
+
+const App = () => {
+    return (
+        <>
+            <CounterCard number="1000"/>
+            <CounterCard number="100"/>
+            <CounterCard number="9"/>
+        </>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Importeren React 
+import React from "react";
+
+// Importeren React DOM
+import ReactDOM from "react-dom/client";
+
+// Importeert App
+import App from "./App/App";
+
+// Variable aan die we koppelen aan het html element root 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// Render de variable
+root.render (
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
